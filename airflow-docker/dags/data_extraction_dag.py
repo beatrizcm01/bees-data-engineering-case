44fb6fd9c4caafbdae52684c38a882844bfff331
Beatriz Menezes Baroni <99352266+beatrizcm01@users.noreply.github.com>
2025-05-04 18:01:08 -0300

    Reorganized files

--- airflow-docker/dags/data_extraction_dag.py
+++ airflow-docker/dags/data_extraction_dag.py
@@ -1,23 +1,30 @@
-from airflow.decorators import dag, task
+from airflow.sdk import dag, task, chain
 from datetime import datetime, timedelta
 import subprocess
 import os
 
-# Function to run the Python script
-@task
-def run_python_script():
-    script_path = os.path.join(os.path.dirname(__file__), 'scripts/bronze-layer', 'data-extraction.py')
-    subprocess.run(['python', script_path], check=True)
+# Run script to extract data and persist it raw into S3 bucket 
 
-# Define the DAG using @dag decorator
 @dag(
     schedule='@daily',  # Run every day at midnight
     start_date=datetime(2025, 5, 2),  # Set start date to today or whenever you want
     catchup=False,  # To avoid running the DAG for past dates
-    default_args={'retries': 1, 'retry_delay': timedelta(minutes=5)}
+    default_args={'retries': 1, 'retry_delay': timedelta(minutes=10)}
 )
-def daily_python_script_dag():
-    run_python_script()  # Call the task within the DAG
 
-# This will define the DAG automatically
-dag_instance = daily_python_script_dag()
+def run_dataflow_dag():
+    @task
+    def run_bronze_layer_script():
+        script_path = os.path.join(os.path.dirname(__file__), 'scripts/bronze-layer', 'data-extraction.py')
+        subprocess.run(['python', script_path], check=True)
+
+    # Run script that transforms data from bronze layer and load it into silver S3 bucket
+
+    @task
+    def run_silver_layer_script():
+        script_path = os.path.join(os.path.dirname(__file__), 'scripts/silver-layer', 'data-transformation.py')
+        subprocess.run(['python', script_path], check=True) 
+    
+    chain(run_bronze_layer_script(), run_silver_layer_script())
+
+dag_instance = run_dataflow_dag()
---
+++ airflow-docker/dags/scripts/gold-layer/view-creation.py
@@ -0,0 +1,33 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import count
+from datetime import datetime
+
+# passes parameters to fetch data from S3
+
+folder_name = datetime.today().strftime('%Y-%m-%d')
+destination_bucket_name = 'openbrewerydb-gold-layer'
+origin_bucket_name = 'openbrewerydb-silver-layer'
+
+# Creates spark session
+
+spark = SparkSession.builder \
+    .appName("DataFrame") \
+    .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.520") \
+    .config("spark.hadoop.fs.s3a.aws.credentials.provider", 
+            "com.amazonaws.auth.DefaultAWSCredentialsProviderChain") \
+    .getOrCreate()
+
+# Reading data into DataFrame
+
+df = spark.read.parquet(f"s3a://{origin_bucket_name}/{folder_name}/")
+
+# Performing aggregation per location and brewery type
+
+brewery_agg_vw = df.groupBy("brewery_type", "country", "region").agg(count("id").alias("brewery_count")).show()
+
+# Saving view in Delta Lake
+# Storing data in S3
+
+brewery_agg_vw.write.format("parquet").saveAsTable("vw_openbrewerydb_gold")
+brewery_agg_vw.write.parquet(f"s3a://{destination_bucket_name}/{folder_name}",
+                 mode='overwrite')
